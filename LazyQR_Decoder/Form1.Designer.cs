89c92fba52823fc3528b3f7a318cd068c2d30ff1
orertrr <orertrr6207@gmail.com>
2023-09-10 16:02:56 +0800

    Add project files.

---
+++ LazyQR_Decoder/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace LazyQR_Decoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_information = new System.Windows.Forms.TextBox();
+            this.btn_excute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_information
+            // 
+            this.txt_information.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_information.Location = new System.Drawing.Point(12, 83);
+            this.txt_information.Multiline = true;
+            this.txt_information.Name = "txt_information";
+            this.txt_information.ReadOnly = true;
+            this.txt_information.Size = new System.Drawing.Size(636, 355);
+            this.txt_information.TabIndex = 0;
+            // 
+            // btn_excute
+            // 
+            this.btn_excute.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_excute.Location = new System.Drawing.Point(12, 12);
+            this.btn_excute.Name = "btn_excute";
+            this.btn_excute.Size = new System.Drawing.Size(636, 65);
+            this.btn_excute.TabIndex = 1;
+            this.btn_excute.Text = "Get and Decode";
+            this.btn_excute.UseVisualStyleBackColor = true;
+            this.btn_excute.Click += new System.EventHandler(this.btn_excute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 450);
+            this.Controls.Add(this.btn_excute);
+            this.Controls.Add(this.txt_information);
+            this.Name = "Form1";
+            this.Text = "Lazy QR Decoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_information;
+        private System.Windows.Forms.Button btn_excute;
+    }
+}
+
---
+++ LazyQR_Decoder/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ZXing.QrCode;
+using ZXing.Common;
+
+namespace LazyQR_Decoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_excute_Click(object sender, EventArgs e)
+        {
+            if (!Clipboard.ContainsImage())
+            {
+                MessageBox.Show("You must clip the image first.");
+                return;
+            }
+
+            Image image = Clipboard.GetImage();
+            Clipboard.SetImage(image);
+            Bitmap bitmap = new Bitmap(image);
+            // bitmap = NoiseReduction(bitmap);
+
+            ZXing.LuminanceSource source = new ZXing.BitmapLuminanceSource(bitmap);
+            HybridBinarizer binarizer = new HybridBinarizer(source);
+            ZXing.BinaryBitmap ZXbitmap = new ZXing.BinaryBitmap(binarizer);
+            
+            QRCodeReader reader = new QRCodeReader();
+            ZXing.Result result = reader.decode(ZXbitmap);
+
+            if (result == null)
+            {
+                MessageBox.Show("Decode fail.");
+                return;
+            }
+
+            txt_information.Text = result.Text;
+        }
+
+        //private Bitmap NoiseReduction(Bitmap origin)
+        //{
+        //    Bitmap result = new Bitmap(origin.Width, origin.Height);
+        //    Color[] gray_pixels = new Color[origin.Width * origin.Height];
+        //    Color[] median_pixels = new Color[origin.Height * origin.Width];
+
+        //    // GrayScale
+        //    for (int i = 0; i < origin.Height; i++)
+        //    {
+        //        for (int j = 0; j < origin.Width; j++)
+        //        {
+        //            Color pixel = origin.GetPixel(j, i);
+        //            int gray = pixel.R / 3 + pixel.G / 3 + pixel.B / 3;
+
+        //            if (gray > 150)
+        //                gray = 255;
+        //            else
+        //                gray = 0;
+
+        //            gray_pixels[i * origin.Width + j] = Color.FromArgb(gray, gray, gray);
+        //            result.SetPixel(j, i, gray_pixels[i * origin.Width + j]);
+        //        }
+        //    }
+
+        //    const int filterSize = 9;
+
+        //    //Median Fileter
+        //    //for (int i = 0; i < origin.Height; i++)
+        //    //{
+        //    //    for (int j = 0; j < origin.Width; j++)
+        //    //    {
+        //    //        byte[] mask = new byte[filterSize * filterSize];
+
+        //    //        for (int filter_i = 0; filter_i < filterSize; filter_i++)
+        //    //        {
+        //    //            for (int filter_j = 0; filter_j < filterSize; filter_j++)
+        //    //            {
+        //    //                int location_i = i - filterSize / 2;
+        //    //                int location_j = j - filterSize / 2;
+        //    //                if (location_i < 0)
+        //    //                    location_i = 0 + (0 - location_i);
+        //    //                else if (location_i >= origin.Height)
+        //    //                    location_i = origin.Height - 1 + (origin.Height - 1 - location_i);
+        //    //                if (location_j < 0)
+        //    //                    location_j = 0 + (0 - location_j);
+        //    //                else if (location_j >= origin.Width)
+        //    //                    location_j = origin.Width - 1 + (origin.Width - 1 - location_j);
+
+        //    //                mask[filter_i * filterSize + filter_j] = gray_pixels[location_i * origin.Width + location_j].R;
+        //    //            }
+        //    //        }
+
+        //    //        Array.Sort(mask);
+        //    //        Color result_color = Color.FromArgb(mask[(filterSize * filterSize) / 2], mask[(filterSize * filterSize) / 2], mask[(filterSize * filterSize) / 2]);
+        //    //        result.SetPixel(j, i, result_color);
+        //    //    }
+        //    //}
+
+        //    return result;
+        //}
+    }
+}
